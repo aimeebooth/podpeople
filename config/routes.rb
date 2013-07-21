f964245a6139e7f1a00809213bf77c8f20dd11fd
Olivier Lacan <olivier.lacan@gmail.com>
2013-07-21 16:41:18 -0400

    Add root route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Podpeople::Application.routes.draw do
   resources :networks do
     resources :shows
   end
+
+  root to: "networks#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130630013127) do
+ActiveRecord::Schema.define(version: 20130630013335) do
 
   create_table "episodes", force: true do |t|
     t.text     "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20130630013127) do
     t.text     "time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "episode_id"
   end
 
   create_table "topics", force: true do |t|

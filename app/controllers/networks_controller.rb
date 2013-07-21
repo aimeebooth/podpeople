f289a977f2d3df9c742dd4b1165b53c2d68cf5d9
Olivier Lacan <olivier.lacan@gmail.com>
2013-07-21 17:38:17 -0400

    Refactor networks controller.
    - use local instead of instance variable to foster good delegation habits
    - explicit (if un-DRY) render calls for each action
    - permit a :description key to be assigned in params
    - display descriptions in :index an :show views
    - use a resource-based form_for instead of an action-based one
    - display error messages in the form when required fields are missing

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -2,56 +2,59 @@ class NetworksController < ApplicationController
   before_filter :find_network, only: [:show, :edit, :update, :destroy]
 
   def show
+    render :show, locals: { network: find_network }
   end
 
   def index
-    @networks = Network.all
+    render :index, locals: { networks: Network.all }
   end
 
   def edit
-    
+    render :edit, locals: { network: find_network }
   end
 
   def new
-    @network = Network.new
+    render :new, locals: { network: Network.new }
   end
 
   def create
-   @network = Network.new(network_params)
+   network = Network.new(network_params)
 
-    if @network.save
+    if network.save
       flash[:notice] = "Network successfully created"
       redirect_to networks_path
     else
       flash[:warning] = "Error creating network"
-      render :new
+      render :new, locals: { network: network }
     end
   end
 
   def update
-    if @network.update_attributes(network_params)
-      redirect_to network_path(@network), notice: "Network successfully updated"
+    network = find_network
+    if network.update_attributes(network_params)
+      redirect_to network_path(network), notice: "Network successfully updated"
     else
       flash[:warning] = "Error creating network"
-      render :edit
+      render :edit, locals: { network: network }
     end
   end
 
   def destroy
-    if @network.destroy
+    network = find_network
+    if network.destroy
       redirect_to networks_path, notice: "Network successfully destroyed"
     else
-      redirect_to network_path(@network), warning: "Network could not be destroyed"
+      redirect_to network_path(network), warning: "Network could not be destroyed"
     end
   end
 
   private
 
   def find_network
-    @network = Network.find(params[:id])
+    Network.find(params[:id])
   end
 
   def network_params
-    params.require(:network).permit(:name)
+    params.require(:network).permit([:name, :description])
   end
 end

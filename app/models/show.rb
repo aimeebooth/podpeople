2a37446a64bbac6c54f5ad0ce411fa6ebbd21bb5
Olivier Lacan <olivier.lacan@gmail.com>
2013-07-21 17:19:13 -0400

    Connect shows to guests with through association.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :episodes
   has_many :hosts
+  has_many :guests, through: :episodes
 
   validates :name, presence: true
   validates :description, presence: true

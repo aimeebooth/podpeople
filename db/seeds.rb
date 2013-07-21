cad3d7e2f247c579e4b434a0aae4543aacf54a76
Olivier Lacan <olivier.lacan@gmail.com>
2013-07-21 17:38:49 -0400

    Create seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+network = Network.create!(
+  name: "Hypothetical", 
+  description: "A hypothetical podcasting network"
+)
+show = Show.create!(
+  name: "A Hypothetical Movie Podcast", 
+  description: "Nerds talking about movies & stuff",
+  network: network
+)
+episode = Episode.create!(
+  title: "Django Unchained - Balls Make Me Sentimental",
+  description: "We discuss Django Unchained. At nine minutes (of 114) Eric mentions that we’re off topic. Just so you know what ...",
+  show: show
+)
+host = Host.create!(
+  name: "Joshua Blount",
+  show: show
+)
+guest = Guest.create!(
+  name: "Brandon Barnes",
+  episode: episode
+)
+topic = Topic.create!(
+  name: "Django Unchained",
+  episode: episode
+)
+taping = Taping.create!(
+  date: 1.weeks.from_now,
+  time: "7PM EDT",
+  episode: episode
+)
